32d894d6df8db723cb36174b362de8aa7435ec98
Richard Bouvet <bouvet.richard@hotmail.fr>
2019-08-07 16:54:45 +0200

    stylax

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |flat|
+  flat = Flat.new(
+    name: Faker::Kpop.boy_bands,
+    address: Faker::Address.city,
+    description: Faker::Lorem.paragraph,
+    price_per_night: rand(30..120),
+    number_of_guests: rand(1..10)
+  )
+  flat.save
+end
+puts "created"
