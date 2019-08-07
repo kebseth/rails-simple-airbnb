6b036f982871682c5b5467b2994599abd2673315
Richard Bouvet <bouvet.richard@hotmail.fr>
2019-08-07 17:08:54 +0200

    pause

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,4 +2,20 @@ class FlatsController < ApplicationController
   def index
     @flats = Flat.all
   end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+    redirect_to root_path
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name,:address, :description, :price_per_night, :number_of_guests)
+  end
 end
